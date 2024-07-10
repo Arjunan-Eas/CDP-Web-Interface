f5ebae1cb18e3055e66fb951adc8b27dc7161463
Arjunan Easwarachandran <arjunan.eas@gmail.com>
2024-07-09 20:56:56 -0700

    Modified app to follow specified file formats

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,9 +10,12 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     text1 = request.form['clientId']
-    text2 = request.form['message']
+    text2 = request.form['topic']
+    text3 = request.form['message']
+    text4 = str(datetime.now())[:10]
+    text5 = str(datetime.now())[11:19]
     with open('/home/ubuntu/webapp/messaging/message.txt', 'w') as f:
-        f.write(f'ClientID: {text1}\nMessage: {text2}')
+        f.write(f'DUID:{text1}\nTOPIC:{text2}\nDATA:{text3}\nDATE:{text4}\nTIME:{text5}')
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
