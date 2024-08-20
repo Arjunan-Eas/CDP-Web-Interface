abca05c38009012ebd8315adba12a837bb33e7d3
Arjunan Easwarachandran <arjunan.eas@gmail.com>
2024-08-19 23:19:20 -0700

    v2

--- webInstall.sh
+++ webInstall.sh
@@ -2,10 +2,13 @@ sudo apt install python3-pip
 
 sudo rm README.md
 sudo rm -r Archive
+sudo mv webapp /
+sudo mv webInstall.sh /
+cd
 
 cd webapp
-python3 -m venv env
+python3 -m venv web_py_env
+source web_py_env/bin/activate
 pip install redis
 pip install flask
 pip install flask-socketio
-
